4fbe5347eca298522c3c4d525cb4f24ed9dae5fe
Mikhail Vasilenko <mvasilenko@gmail.com>
2017-03-22 17:35:45 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 IMAGE="mvasilenko/telegram-bot-kievradar"
 DIR=`pwd`
-docker ps | grep $IMAGE | awk '{print $1}' | xargs docker stop
 docker pull $IMAGE
-docker run --rm -d --env-file $DIR/.env $IMAGE
+docker ps | grep $IMAGE | awk '{print $1}' | xargs docker stop
+docker run -d --rm --env-file $DIR/.env $IMAGE
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc) 2>/dev/null
