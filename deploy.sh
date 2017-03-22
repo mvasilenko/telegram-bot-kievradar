f1fd7a8d2e9f76135c6bf466bbd194d3ba6d5707
Mikhail Vasilenko <mvasilenko@gmail.com>
2017-03-22 14:39:39 +0200

    add tests

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 IMAGE="mvasilenko/telegram-bot-kievradar"
+DIR=`pwd`
 docker ps | grep $IMAGE | awk '{print $1}' | xargs docker stop
 docker pull $IMAGE
-docker run -d --env-file ~/telegram-bot-kievradar/.env $IMAGE
+docker run --rm -d --env-file $DIR/.env $IMAGE
--- run-docker-hook.sh
+++ run-docker-hook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .env
-nohup ./docker-hook -t $DOCKER_HOOK_TOKEN -c deploy.sh &
+nohup ./docker-hook -t $DOCKER_HOOK_TOKEN -c ./deploy.sh &
 
