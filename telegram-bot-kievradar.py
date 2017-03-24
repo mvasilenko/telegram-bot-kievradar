eb5815c876a0cb9735f5113bc0e438caa34677e2
Mikhail Vasilenko <mvasilenko@gmail.com>
2017-03-24 16:34:06 +0200

    fixh

--- telegram-bot-kievradar.py
+++ telegram-bot-kievradar.py
@@ -179,12 +179,12 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+#    updater.start_polling()
 
     # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+#    updater.idle()
 
 
 if __name__ == '__main__':
